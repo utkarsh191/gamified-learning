7ea119109acdc5e0d9a72abbed92decada08861e
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-08-27 23:46:34 +0530

    Setup frontend with React TypeScript Tailwind Router and Axios

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Gamified Learning
+      </h1>
+    </div>
+  )
+}
+
+export default App
