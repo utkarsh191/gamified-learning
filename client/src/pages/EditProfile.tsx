b2cd26ae1fa330667baf748fbb9ac96e1b8de83b
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-08-30 00:41:41 +0530

    Add X username field to edit profile

--- client/src/pages/EditProfile.tsx
+++ client/src/pages/EditProfile.tsx
@@ -97,6 +97,19 @@ function EditProfile() {
               className="w-full rounded-lg border border-gray-700 bg-gray-800 px-4 py-3 text-white outline-none transition focus:border-blue-500"
             />
           </div>
+
+          {/* X Username */}
+          <div className="mt-5">
+            <label className="mb-2 block font-semibold">
+              X Username
+            </label>
+
+            <input
+              type="text"
+              placeholder="Enter your X username"
+              className="w-full rounded-lg border border-gray-700 bg-gray-800 px-4 py-3 text-white outline-none transition focus:border-blue-500"
+            />
+          </div>
         </section>
 
       </div>
