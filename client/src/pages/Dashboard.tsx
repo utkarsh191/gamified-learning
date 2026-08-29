10c7d5a462b7d319e32ae64bc4aa4491bacff8ee
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-08-29 22:03:08 +0530

    Create dashboard navbar

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,9 +1,11 @@
 function Dashboard() {
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Welcome to Dashboard 🚀
-      </h1>
+    <div className="min-h-screen bg-gray-900">
+      
+      <nav>
+        
+      </nav>
+
     </div>
   );
 }
