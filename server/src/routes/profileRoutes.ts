50d3598fed890abb092053ab322bdd27470c77ea
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-08-30 01:58:36 +0530

    Protect profile update route with user authentication

--- server/src/routes/profileRoutes.ts
+++ server/src/routes/profileRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express";
 
+import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-router.put("/", (req, res) => {
+router.put("/", protect, (req, res) => {
+  console.log(req.user);
   console.log(req.body);
 
   res.status(200).json({
