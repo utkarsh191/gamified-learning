a4768cb27233f4c8362f3d30827dde458732b630
UTKARSH KESHARWANI <utkarsh98399426@gmail.com>
2026-08-31 22:41:38 +0530

    feat: add get profile route

--- server/src/routes/profileRoutes.ts
+++ server/src/routes/profileRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-
 import { AuthRequest, protect } from "../middleware/authMiddleware.js";
+import { getMe } from "../controllers/userController.js";
 
 const router = express.Router();
+
+router.get("/", protect, getMe);
 
 router.put("/", protect, (req: AuthRequest, res) => {
   console.log(req.user);
